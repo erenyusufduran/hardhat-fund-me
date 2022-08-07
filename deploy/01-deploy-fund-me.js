7a529ea78719711673cdde1d203f7d0cee0190a8
Eren Yusuf Duran <erenyusufduran@gmail.com>
2022-08-08 02:35:56 +0300

    mocking, working with different versions, networks

--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -1,7 +1,28 @@
 const { network } = require("hardhat");
+const {
+  networkConfig,
+  developmentChains,
+} = require("../helper-hardhat-config");
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy, log } = deployments;
   const { deployer } = await getNamedAccounts();
   const chainId = network.config.chainId;
+
+  let ethUsdPriceFeedAddress;
+  if (developmentChains.includes(network.name)) {
+    const ethUsdAggregator = await deployments.get("MockV3Aggregator");
+    ethUsdPriceFeedAddress = ethUsdAggregator.address;
+  } else {
+    ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"];
+  }
+
+  const fundMe = await deploy("FundMe", {
+    from: deployer,
+    args: [ethUsdPriceFeedAddress], // put the price feed address
+    log: true,
+  });
+  log("---------------------------");
 };
+
+module.exports.tags = ["all", "fundme"];
--- hardhat.config.js
+++ hardhat.config.js
@@ -13,7 +13,9 @@ const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.8",
+  solidity: {
+    compilers: [{ version: "0.8.8" }, { version: "0.6.6" }],
+  },
   defaultNetwork: "hardhat",
   networks: {
     ropsten: {
@@ -25,23 +27,23 @@ module.exports = {
       accounts: [PRIVATE_KEY],
       chainId: 4,
     },
-    // gasReporter: {
-    //   enabled: true,
-    //   outputFile: "gas-report.txt",
-    //   noColors: true,
-    //   currency: "USD",
-    //   coinmarketcap: COINMARKETCAP_API_KEY,
-    // },
-    // etherscan: {
-    //   apiKey: ETHERSCAN_API_KEY,
-    // },
-    // namedAccounts: {
-    //   deployer: {
-    //     default: 0,
-    //   },
-    //   user: {
-    //     default: 1,
-    //   },
-    // },
+  },
+  gasReporter: {
+    enabled: true,
+    outputFile: "gas-report.txt",
+    noColors: true,
+    currency: "USD",
+    coinmarketcap: COINMARKETCAP_API_KEY,
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+    user: {
+      default: 1,
+    },
   },
 };
