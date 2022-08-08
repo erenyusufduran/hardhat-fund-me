de58051851324ddce105e1d2d1e51c0845ac5cf6
Eren Yusuf Duran <erenyusufduran@gmail.com>
2022-08-08 22:09:23 +0300

    gas optimization, refactoring with getters

--- test/unit/FundMe.test.js
+++ test/unit/FundMe.test.js
@@ -17,7 +17,7 @@ describe("FundMe", async function () {
 
   describe("constructor", async function () {
     it("Sets the aggregator addresses correctly.", async function () {
-      const response = await fundMe.s_priceFeed();
+      const response = await fundMe.getPriceFeed();
       assert.equal(response, mockV3Aggregator.address);
     });
   });
@@ -30,12 +30,12 @@ describe("FundMe", async function () {
     });
     it("Updated the amount funded data structure", async function () {
       await fundMe.fund({ value: sendValue });
-      const response = await fundMe.s_addressToAmountFunded(deployer);
+      const response = await fundMe.getAddressToAmountFunded(deployer);
       assert.equal(response.toString(), sendValue.toString());
     });
     it("Adds funder to array of funders", async function () {
       await fundMe.fund({ value: sendValue });
-      const funder = await fundMe.s_funders(0);
+      const funder = await fundMe.getFunder(0);
       assert.equal(funder, deployer);
     });
   });
@@ -97,11 +97,11 @@ describe("FundMe", async function () {
         endingDeployerBalance.add(gasCost).toString()
       );
 
-      await expect(fundMe.s_funders(0)).to.be.reverted;
+      await expect(fundMe.getFunder(0)).to.be.reverted;
 
       for (i = 1; i < 6; i++) {
         assert.equal(
-          await fundMe.s_addressToAmountFunded(accounts[i].address),
+          await fundMe.getAddressToAmountFunded(accounts[i].address),
           0
         );
       }
@@ -167,11 +167,11 @@ describe("FundMe", async function () {
         endingDeployerBalance.add(gasCost).toString()
       );
 
-      await expect(fundMe.s_funders(0)).to.be.reverted;
+      await expect(fundMe.getFunder(0)).to.be.reverted;
 
       for (i = 1; i < 6; i++) {
         assert.equal(
-          await fundMe.s_addressToAmountFunded(accounts[i].address),
+          await fundMe.getAddressToAmountFunded(accounts[i].address),
           0
         );
       }
